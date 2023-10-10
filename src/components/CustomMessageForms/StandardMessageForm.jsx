821b6fbaf363a53e7c542fc8dbbb82c447a1ea7d
Gordon D <gordon.draine97@yahoo.com>
2023-10-09 22:31:20 -0400

    setting preview settings for standard message

--- src/components/CustomMessageForms/StandardMessageForm.jsx
+++ src/components/CustomMessageForms/StandardMessageForm.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import React, { useState } from "react";
 
 const StandardMessageForm = () => {
-  return <div>StandardMessageForm</div>;
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const [preview, setPreview] = useState("");
+
+  return (
+    <div className="message-from-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            alt=" message from preview "
+            className="message-form-preview"
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+            <XMarkIcon 
+             className="message-form-icon"
+             onClick={() => {
+                setPreview("");
+                setAttachment("");
+             }}
+            />
+
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default StandardMessageForm;
