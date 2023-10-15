5ccb0ffa3101241905fcfffa4fd056086367922c
Gordon D <gordon.draine97@yahoo.com>
2023-10-14 22:18:09 -0400

    adding open AI api to project

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 
+// configurations 
 dotenv.config();
 const app = express();
 app.use(express.json);
@@ -15,9 +16,21 @@ app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors);
 
+// open Ai configuration 
+const openai = new OpenAI({
+    apiKey: 'OPEN_API_KEY', // defaults to process.env["OPENAI_API_KEY"]
+});
+
+async function main() {
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: 'Say this is a test' }],
+        model: 'gpt-3.5-turbo',
+
+
 // server startup
 
-const PORT  = process.env.PORT ||  8080;
+const PORT  = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
+
