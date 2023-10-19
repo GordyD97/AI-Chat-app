be2565d36fc47b16565ac40e91a899a5ff2b4f45
Gordon D <gordon.draine97@yahoo.com>
2023-10-18 21:25:03 -0400

    setting up redux toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query"; 
+import { api } from "@/state/api";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+export const store = configureStore({
+    reducer: { [api.reducerPath]: api.reducer },
+    middleware: (getDefault) => getDefault().concat(api.middleware)
+});
+setupListeners(store.dispatch);
+
+// the block above is the default set up for redux toolkit. 
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
