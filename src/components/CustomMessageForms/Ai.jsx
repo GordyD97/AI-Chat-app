03c3bdbabb7caeb69c14b223a4b42aba2f272681
Gordon D <gordon.draine97@yahoo.com>
2023-10-23 23:25:38 -0400

    fixing ai render error

--- src/components/CustomMessageForms/Ai.jsx
+++ src/components/CustomMessageForms/Ai.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MessageFormUI from './MessageFormUI';
 
-const Ai = () => {
+const Ai = ({ props, activeChat }) => {
+    const StandardMessageForm = ({ props, activeChat }) => {
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const [preview, setPreview] = useState("");
+
+  const handleChange = (e) => setMessage(e.target.value);
+
+  const handleSubmit = async () => {
+    const date = new Date()
+      .toISOString()
+      .replace("T", " ")
+      .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+    const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+    const form = {
+      attachment: at,
+      create: date,
+      sender_username: props.username,
+      text: message,
+      activeChatId: activeChat.id,
+    };
+
+    props.onSubmit(form);
+    setMessage("");
+    setAttachment("");
+  };
   return (
-    <div>Ai</div>
-  )
+    <MessageFormUI
+      setAttachment={setAttachment}
+      message={message}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  );
 }
 
 export default Ai
--- src/components/CustomMessageForms/StandardMessageForm.jsx
+++ src/components/CustomMessageForms/StandardMessageForm.jsx
@@ -1,10 +1,6 @@
-import {
-  PaperAirplaneIcon,
-  PaperClipIcon,
-  XMarkIcon,
-} from "@heroicons/react/24/solid";
+
 import React, { useState } from "react";
-import Dropzone from "react-dropzone";
+
 import MessageFormUI from "./MessageFormUI";
 
 
